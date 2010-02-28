00f63d8b512fcd29df9c7d22b56c83cda4c7acf4
Josh Wolfe <thejoshwolfe@gmail.com>
2010-02-28 12:14:04 -0700

    checking for --theRealPath

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <QtGui/QApplication>
 #include "MainWindow.h"
 
+char * getTheRealPath(int argc, char *argv[]);
+void copySelf();
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    char * theRealPath = getTheRealPath(argc, argv);
+    if (theRealPath == NULL) {
+        copySelf();
+        return 0;
+    } else {
+        // load application
+        // TODO: use theRealPath
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+}
+
+char * getTheRealPath(int argc, char *argv[]) {
+    if (argc < 3)
+        return NULL;
+    char * dashDashTheRealPath = argv[1];
+    if (strcmp(dashDashTheRealPath, "--theRealPath") != 0)
+        return NULL;
+    return argv[2];
+}
+
+void copySelf() {
+    // TODO
 }

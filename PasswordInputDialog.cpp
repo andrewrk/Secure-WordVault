411a06561196651715fa725e27bee56179c98530
David Bendit <David@ibendit.com>
2010-04-15 03:23:54 -0700

    Addressing changes requested for Password Input Dialog in PDF from 4/15/10

--- PasswordInputDialog.cpp
+++ PasswordInputDialog.cpp
@@ -8,7 +8,15 @@ PasswordInputDialog::PasswordInputDialog(QString filename, QWidget *parent) :
     m_ui(new Ui::PasswordInputDialog)
 {
     m_ui->setupUi(this);
-    m_ui->lblFile->setText(tr("File: ") + QFileInfo(filename).completeBaseName());
+    //m_ui->lblFile->setText(tr("File: ") + QFileInfo(filename).fileName());
+    m_ui->lblFileName->setText(QFileInfo(filename).fileName());
+
+    QIcon icon = windowIcon();
+    Qt::WindowFlags flags = windowFlags();
+    Qt::WindowFlags helpFlag = Qt::WindowContextHelpButtonHint;
+    flags = flags & (~helpFlag);
+    setWindowFlags(flags);
+    setWindowIcon(icon);
 }
 
 PasswordInputDialog::~PasswordInputDialog()
